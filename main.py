62a8230b987e9a8b3bf53c0d44a5f27d028a729a
Niiii <tapioka2real@gmail.com>
2024-10-22 16:09:30 +0900

    匿名意見箱がうまく読み込めていないバグを修正

--- main.py
+++ main.py
@@ -91,6 +91,10 @@ async def check_for_updates():
     except Exception as e:
         print(f"シートの更新チェック中にエラーが発生しました: {e}")
 
+# タスクを開始する前に、すでに実行中か確認
+if not check_for_updates.is_running():
+    check_for_updates.start()
+
 @bot.tree.command(name="help", description="コマンドの詳細表示")
 async def help(interaction: discord.Interaction):
     embed = discord.Embed(title="コマンド一覧", color=discord.Colour.purple())

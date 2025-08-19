26bb24eddc9bfeb6d2712df800650e1b69292df3
Niiii <tapioka2real@gmail.com>
2025-08-19 23:33:05 +0900

    [update] configuration files

--- server.py
+++ server.py
@@ -1,17 +1,13 @@
-from threading import Thread
-
 from fastapi import FastAPI
 import uvicorn
+import os
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-	return {"message": "Server is Online."}
+    return {"message": "Server is Online."}
 
-def start():
-	uvicorn.run(app, host="0.0.0.0", port=8080)
-
-def server_thread():
-	t = Thread(target=start)
-	t.start()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
